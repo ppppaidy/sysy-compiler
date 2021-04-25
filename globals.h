2fd7acb565c9f813d9dfc7422f48ee4f543d31e7
ppppaidy <ppppaidy@pku.edu.cn>
2021-04-25 22:25:11 +0800

    create globals.h

---
+++ globals.h
@@ -0,0 +1,16 @@
+// globals.h
+// define global signs
+
+#ifndef _GLOBALS_H_
+#define _GLOBALS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int sysy2eeyore = 0;
+int eeyore2tigger = 0;
+int tigger2riscv = 0;
+
+#endif
