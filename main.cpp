7371a2db466ad510947d561689369a4c600d3120
ppppaidy <ppppaidy@pku.edu.cn>
2021-04-25 22:24:39 +0800

    create main.cpp

---
+++ main.cpp
@@ -0,0 +1,74 @@
+// main.c
+// handle commond line imput
+
+#include "globals.h"
+
+char err_usage[] = "usage : %s -S [-e|-t] <filename>.[c|eeyore|tigger] -o <filename>.S\n";
+
+char filename[256], filename_o[256];
+char input_filename[256];
+char output_filename[256];
+
+int main(int argc, char* argv[]){
+    if(argc < 5){
+        fprintf(stderr, err_usage, argv[0]);
+        return 1;
+    }
+    if(strcmp(argv[1], "-S") != 0){
+        fprintf(stderr, err_usage, argv[0]);
+        return 1;
+    }
+
+    if(strcmp(argv[2], "-e") != 0 && strcmp(argv[2], "-t") != 0){
+        strcpy(filename, argv[2]);
+    }
+    else{
+        strcpy(filename, argv[3]);
+    }
+
+    if(strcmp(strchr(filename, '.')+1, "c") == 0){
+        sysy2eeyore = eeyore2tigger = tigger2riscv = 1;
+    }
+    else if(strcmp(strchr(filename, '.')+1, "eeyore") == 0){
+        eeyore2tigger = tigger2riscv = 1;
+    }
+    else if(strcmp(strchr(filename, '.')+1, "tigger") == 0){
+        tigger2riscv = 1;
+    }
+    else{
+        fprintf(stderr, err_usage, argv[0]);
+        return 1;
+    }
+    *strchr(filename, '.') = '\0';
+
+    if(strcmp(argv[2], "-e") == 0){
+        eeyore2tigger = tigger2riscv = 0;
+    }
+    if(strcmp(argv[2], "-t") == 0){
+        tigger2riscv = 0;
+    }
+
+    if(strcmp(argv[3], "-o") == 0){
+        strcpy(filename_o, argv[4]);
+    }
+    else{
+        strcpy(filename_o, argv[5]);
+    }
+
+    //sysy to eeyore
+    if(sysy2eeyore == 1){
+
+    }
+
+    //eeyore to tigger
+    if(eeyore2tigger == 1){
+
+    }
+
+    //tigger to riscv
+    if(tigger2riscv == 1){
+
+    }
+    
+    return 0;
+}
